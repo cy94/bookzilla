2200a59d3de2e649ad9a2b38ca300d37cf535402
Ananth Murthy <ananth956@gmail.com>
2015-03-25 22:15:13 +0530

    Moved login links to nav bar, added contact and about pages.

--- bookzilla/urls.py
+++ bookzilla/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from bookzilla import views
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'bookzilla.views.index', name='index'),
     url(r'^users/', include('users.urls', namespace='users')),
-  
+    url(r'^about/', views.about, name='about' ),
+    url(r'^contact/', views.contact, name='contact'),
+  	
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- bookzilla/views.py
+++ bookzilla/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def contact(request):
+	return render(request, 'contact.html')
