83c1e7b4bb4a16c646672148004e0a90fa432c45
Chandan <chandan.yeshwanth@gmail.com>
2015-03-26 12:49:12 +0530

    added password=confirm password JS validation to register page

--- users/static/js/register.js
+++ users/static/js/register.js
@@ -1,6 +1,24 @@
 // register.js
 
 $(document).ready(function(){
+	$('#password, #confirmpassword').focusout(function(){
+		if($(this).find('input').val().length == 0){
+			$(this).removeClass('has-success');		
+			$(this).addClass('has-error');	
+		}
+		else{
+			$(this).removeClass('has-error');	
+			$(this).addClass('has-success');		
+		}
+
+		if($('#password').find('input').val() != $('#confirmpassword').find('input').val()){
+			$('#password, #confirmpassword').addClass('has-error');
+		}
+		else{
+			$('#password, #confirmpassword').removeClass('has-error');
+		}
+	});
+
 	$('#register-form').on('submit', function(event){
 		event.preventDefault();
 		console.log('register form submitted');
