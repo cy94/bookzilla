c91d583ec6bccd7d60efd5445cbaf07e62ef100a
Ananth Murthy <ananth956@gmail.com>
2015-03-26 14:10:50 +0530

    strated ajax for register form

--- users/static/js/register.js
+++ users/static/js/register.js
@@ -16,32 +16,38 @@ $(document).ready(function(){
 		}
 		else{
 			$('#password, #confirmpassword').removeClass('has-error');
+			$('#password, #confirmpassword').addClass('has-success');
 		}
 	});
 
 	$('#register-form').on('submit', function(event){
 		event.preventDefault();
-		console.log('register form submitted');
+		console.log('Register form submitted.');
 
+
+		var username = $('#username').val();
+	    if (username == ''){
+	       alert('Please enter your username !');
+	       return false;
+	    }
+
+	    var $form = $(this);
 		$.ajax({
-		    url : "register_validate/", // the endpoint
-		    type : "POST", // http method
-		    data : { the_post : $('#post-text').val() }, // data sent with the post request
-
-		    // handle a successful response
-		    success : function(json) {
-		        $('#post-text').val(''); // remove the value from the input
-		        console.log(json); // log the returned json to the console
-		        console.log("success"); // another sanity check
-		    },
-
-		    // handle a non-successful response
-		    error : function(xhr,errmsg,err) {
-		        $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
-		            " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
-		        console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-		    }
-		});
-
+			type: "POST",
+			url: {% url 'users:register_validate' %},
+			data: $form.serialize(),
+			dataType: "json",
+			success: function(response) {
+				if (!response.success) {
+	                alert(response.error);
+	            }else {
+		            alert('This username is available!');
+	            }
+			},
+			error: function(rs, e) {
+				alert(rs.responseText);
+				return false;
+			}
+		}); 
 	});
 })
--- users/views.py
+++ users/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth import login as djlogin
 from django.contrib.auth import logout as djlogout
-
+from django.http import JsonResponse
 # Create your views here
 def login(request):
 	return render(request, 'login.html')
@@ -36,4 +37,16 @@ def register_validate(request):
     # if correct
     # return HttpResponseRedirect(reverse('users:home'))
     # else
-    return HttpResponseRedirect(reverse('users:register'))
+    error = ''
+    success = False
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        if not username:
+            error = 'Please enter your username'
+        elif User.objects.filter(username__exact=username).exists():
+            error = 'Sorry, this username is already taken !'
+        else:
+            success = True
+
+    ajax_vars = {'success': success, 'error': error}
+    return JsonResponse(ajax_vars)
