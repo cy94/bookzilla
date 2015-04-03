37458073c5b1e6c9bed0a26be133ca3b85311bbf
Chandan <chandan.yeshwanth@gmail.com>
2015-04-03 10:18:38 +0530

    added Book model
    added Book model with user, title, author and migrated

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class Book(models.Model):
+	owner = models.ForeignKey(User)
+	title = models.CharField(max_length=50)
+	author = models.CharField(max_length=50)
