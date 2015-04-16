feb3808767a7ddc2409a271fa23b6227dda02f7c
Chandan <chandan.yeshwanth@gmail.com>
2015-04-16 09:26:38 +0530

    added BookRequest model, status choice is incomplete
    status choice is an integer field showing the current status of the request

--- bookrequests/models.py
+++ bookrequests/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+from books.models import Book
+
+class BookRequest(models.Model):
+	WITH_OWNER = 0
+	REQUEST_ACCEPTED = 1
+	WITH_COURIER_TO_BORROWER = 2
+	WITH_BORROWER = 3
+	DONE_READING = 4
+	WITH_COURIER_TO_OWNER = 5
+	RETURNED = 6
+	REQUEST_REJECTED = 7
+
+	STATUS_CHOICES = (
+		(WITH_OWNER , 'with owner')
+		(ACCEPTED , 'accepted')
+		(WITH_COURIER_TO_BORROWER , '')
+		(WITH_BORROWER , '')
+		(DONE_READING , '')
+		(WITH_COURIER_TO_OWNER , '')
+		(RETURNED , '')
+	)		
+
+	book = models.OneToOneField(Book)
+	borrower = models.OneToOneField(User)
+	status = models.IntegerField(default=0, choices=STATUS_CHOICES)
+
