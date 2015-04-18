cf6f707101ab1e0b6d159847715841c014de6100
Chandan <chandan.yeshwanth@gmail.com>
2015-04-18 20:33:19 +0530

    Add books/templatetags and show_book_private tag
    the template tag is used to display a single book, details
    and links as seen by the owner.
    use in a template -
    {% load book_extras %}
    {% show_book_private <book object> %}

---
+++ books/templatetags/book_extras.py
@@ -0,0 +1,25 @@
+from django import template
+
+register = template.Library()
+
+# a partial view of a single book as
+# seen by the owner
+
+# returns the book id, title and author of 'book'
+
+# TODO: check the status of the book
+# and add classes to color the book block
+# and other logic?
+
+@register.inclusion_tag('books/book_private.html')
+def show_book_private(book):
+	return {
+		'book_id': book.id,
+		'book_title': book.title,
+		'book_author': book.author
+	}
+
+# this tag allows the viewer to request the book/
+# etc - as seen by a non-owner
+def show_book_public()
+	pass
